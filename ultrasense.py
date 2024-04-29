dc528d9429235bc0d5a4575f918bb9d1b378143a
lauracalderon1 <111077399+lauracalderon1@users.noreply.github.com>
2024-04-29 13:38:03 -0500

    Stdout con erro 18

--- ultrasense.py
+++ ultrasense.py
@@ -114,10 +114,9 @@ def dijkstra(matriz_energia, lista_numeros_unicos, camino_parejas):
     return caminos, total_ltp
 
 
-
 def main():
     num_casos = int(sys.stdin.readline().strip())
-    print(f"Cantidad de casos: {num_casos}")
+    # print(f"Cantidad de casos: {num_casos}")
 
     for caso in range(1, num_casos + 1):
         n, w1, w2 = map(int, sys.stdin.readline().strip().split())
@@ -126,19 +125,24 @@ def main():
         camino = encontrar_camino_euleriano(grafo)
 
         if camino is None:
-            print(f"Caso {caso}: No se encontró camino euleriano")
+            print(f"NO SE PUEDE")
             continue
 
         unicos = obtener_numeros_unicos(parejas)
         costos = calcular_matriz_energia(unicos, w1, w2)
         rutas, total_ltp = dijkstra(costos, unicos, camino)
 
-        print(f"Caso {caso}: n={n}, w1={w1}, w2={w2}")
+       # print(f"Caso {caso}: n={n}, w1={w1}, w2={w2}")
         resultado = []
         for i, ruta in enumerate(rutas):
-            if i < len(camino) - 1:
-                resultado.append(f"({camino[i][0]},{camino[i][1]}),{','.join(map(str, ruta))}")
+            resultado.append(f"({camino[i][0]},{camino[i][1]}),{','.join(map(str, ruta))}")
+        
+        if len(camino) > len(rutas):
+            ultimo_par = f"({camino[-1][0]},{camino[-1][1]})"
+            resultado.append(ultimo_par)
+
         print(",".join(resultado) + f" {total_ltp}")
 
 if __name__ == "__main__":
     main()
+
